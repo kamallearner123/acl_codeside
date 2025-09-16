e9ab3ff31961bb627c86a273640bac36e12384d5
KamalCazeLabs <kamal@cazelabs.com>
2025-09-16 22:41:04 +0530

    Update settings.py

--- leetcode_clone/settings.py
+++ leetcode_clone/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-##*^*1v*swr1k^#!0z#h#d^snnm(&pat%nbk)%o^@effwwb5+=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", "www.aptcomputinglabs.com"]
 
 
 # Application definition
